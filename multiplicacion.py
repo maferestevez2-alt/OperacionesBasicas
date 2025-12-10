6311087b2dbc7e5acf51d4f2f9350a97337feaf7
alemucino-alt <alemucino70@gmail.com>
2025-12-10 08:37:50 -0600

    Cree un archivo multiplicacion.py

---
+++ multiplicacion.py
@@ -0,0 +1,8 @@
+def multiplica_tres_numeros(a,b,c):
+    return a*b*c
+num1=float(input("Ingresa el primer numero:"))
+num2=float(input("Ingresa el segundo numero: "))
+num3=float(input("Ingresa el tercer numero: "))
+
+resultado= multiplica_tres_numeros(num1,num2,num3)
+print("El resultado de la multilicacion es:",resultado)
--- suma.py
+++ suma.py
@@ -1,2 +1,8 @@
-print ("HOLA")
-print("Alexa")
+def suma_tres_numeros(a,b,c):
+    return a+b+c
+num1=float(input("Ingresa el primer numero:"))
+num2=float(input("Ingresa el segundo numero: "))
+num3=float(input("Ingresa el tercer numero: "))
+
+resultado= suma_tres_numeros(num1,num2,num3)
+print("El resultado de la suma es:",resultado)
